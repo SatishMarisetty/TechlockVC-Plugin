b0abfd86106be1506fd11ebe13ef00bd6456d7a7
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-28 07:10:39 +0530

    Update userbotjoin.py

--- DaisyXMusic/modules/userbotjoin.py
+++ DaisyXMusic/modules/userbotjoin.py
@@ -40,7 +40,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "DaisyMusic"
+        user.first_name = "TECHLOCK"
 
     try:
         await USER.join_chat(invitelink)
@@ -53,7 +53,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            "\n\nOr manually add @TechlockVCmusicassistant to your Group and try again</b>",
         )
         return
     await message.reply_text(
@@ -114,7 +114,7 @@ async def addcchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "DaisyMusic"
+        user.first_name = "TECHLOCK"
 
     try:
         await USER.join_chat(invitelink)
@@ -128,10 +128,9 @@ async def addcchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            "\n\nOr manually add @TechlockVCmusicassistant to your Group and try again</b>",
         )
         return
     await message.reply_text(
         "<b>helper userbot joined your channel</b>",
     )
-    
