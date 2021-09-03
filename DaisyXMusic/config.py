028676b05dc18bb7d550942a628e71e72c2d2bf9
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-09-03 10:18:55 +0530

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -12,7 +12,7 @@
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# along with this program.  see <https://www.gnu.org/licenses/>.
 # Modified by Inukaasith
 
 from os import getenv
